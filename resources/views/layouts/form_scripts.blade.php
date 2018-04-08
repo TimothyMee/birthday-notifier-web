835c03c6c0fd632f7889404d43494f2a7e8937b2
TimothyMee <timothy33.tf@gmail.com>
2018-04-08 18:09:00 +0100

    starting project

---
+++ resources/views/layouts/form_scripts.blade.php
@@ -0,0 +1,9 @@
+<script src="/assets/form_assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="/assets/form_assets/vendor/animsition/js/animsition.min.js"></script>
+<script src="/assets/form_assets/vendor/bootstrap/js/popper.js"></script>
+<script src="/assets/form_assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/form_assets/vendor/select2/select2.min.js"></script>
+<script src="/assets/form_assets/vendor/daterangepicker/moment.min.js"></script>
+<script src="/assets/form_assets/vendor/daterangepicker/daterangepicker.js"></script>
+<script src="/assets/form_assets/vendor/countdowntime/countdowntime.js"></script>
+<script src="/assets/form_assets/js/main.js"></script>
